ADI-Foundation-Labs/ADI-Stack-zkOS

Remove default app.bin, introduce testing.bin

tests/binary_checker/src/lib.rs
@@ -11,7 +11,9 @@ mod tests {
 
         let zksync_os_path =
             std::env::var("ZKSYNC_OS_DIR").unwrap_or_else(|_| String::from("../../zksync_os"));
-        let file_path = PathBuf::from_str(&zksync_os_path).unwrap().join("app.text");
+        let file_path = PathBuf::from_str(&zksync_os_path)
+            .unwrap()
+            .join("testing.text");
         let mut file = std::fs::File::open(file_path).unwrap();
         file.read_to_end(&mut binary).unwrap();
         assert!(binary.len() % 4 == 0);

zksync_os/reproduce/reproduce.sh
@@ -17,7 +17,7 @@ docker build \
 cid="$(docker create --platform=linux/amd64 zksync-os-bin)"
 
 FILES=(
-    app.bin
+    testing.bin
     evm_replay.bin
     server_app.bin
     server_app_logging_enabled.bin

ateirraid94-hub/zksync-os-agent-testing

tests/binary_checker/src/lib.rs
@@ -11,7 +11,9 @@ mod tests {
 
         let zksync_os_path =
             std::env::var("ZKSYNC_OS_DIR").unwrap_or_else(|_| String::from("../../zksync_os"));
-        let file_path = PathBuf::from_str(&zksync_os_path).unwrap().join("app.text");
+        let file_path = PathBuf::from_str(&zksync_os_path)
+            .unwrap()
+            .join("for_tests.text");
         let mut file = std::fs::File::open(file_path).unwrap();
         file.read_to_end(&mut binary).unwrap();
         assert!(binary.len() % 4 == 0);

zksync_os/reproduce/reproduce.sh
@@ -17,7 +17,7 @@ docker build \
 cid="$(docker create --platform=linux/amd64 zksync-os-bin)"
 
 FILES=(
-    app.bin
+    for_tests.bin
     evm_replay.bin
     server_app.bin
     server_app_logging_enabled.bin