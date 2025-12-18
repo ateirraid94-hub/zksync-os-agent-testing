a0230b2bddb0eaad6ac96e93e7db5b9e3d8b1666
Vladislav Volosnikov <Volosnikov.apmath@gmail.com>
2025-12-17 18:40:13 -0600

    Make p256 precompile warm by default

--- evm_interpreter/src/utils.rs
+++ evm_interpreter/src/utils.rs
@@ -168,5 +168,5 @@ pub fn is_precompile(address: &B160) -> bool {
     if limbs[1] != 0u64 || limbs[2] != 0u64 {
         return false;
     }
-    limbs[0] > 0 && limbs[0] <= highest_precompile_address
+    (limbs[0] > 0 && limbs[0] <= highest_precompile_address) || limbs[0] == 100
 }
