4b5e1208763ec6dd2de45c67e72a4c66c20d7338
Raid Ateir <ateirraid@gmail.com>
2026-02-20 15:19:50 +0400

    feat: remove l2_base_token_hook, add update_saved_total_supply during mint

--- system_hooks/src/call_hooks/mod.rs
+++ system_hooks/src/call_hooks/mod.rs
@@ -3,7 +3,6 @@ pub mod mock_precompiles;
 
 pub mod contract_deployer;
 pub mod l1_messenger;
-pub mod l2_base_token;
-pub mod precompiles;
 // TODO: temporary solution, should be removed before the release
 pub mod mint_base_token;
+pub mod precompiles;
