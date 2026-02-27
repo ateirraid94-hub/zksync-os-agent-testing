a28f3e1c95d8e7c172004e2e9aa1b0fc8d34398d
Vladislav Volosnikov <Volosnikov.apmath@gmail.com>
2026-02-27 16:37:25 -0600

    EVM-1167: move MAX_EVENT_TOPICS to system constants

--- zk_ee/src/storage_types/constants.rs
+++
@@ -1,2 +0,0 @@
-/// Maximum number of topics allowed per event.
-pub const MAX_EVENT_TOPICS: usize = 4;
--- zk_ee/src/storage_types/mod.rs
+++ zk_ee/src/storage_types/mod.rs
@@ -1,9 +1,7 @@
 //! Serialization and deserialization helpers for storage-related keys and values.
 
-mod constants;
 mod initial_storage_slot_data;
 mod storage_address;
 
-pub use constants::MAX_EVENT_TOPICS;
 pub use initial_storage_slot_data::InitialStorageSlotData;
 pub use storage_address::StorageAddress;
